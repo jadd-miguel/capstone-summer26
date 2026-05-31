fef137d97fc086d58fc5c5a4afc8ba90f8a45edd
jadd-miguel <i6hx3m@gmail.com>
2026-05-31 19:29:59 -0400

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
-import os
 from supabase import create_client, Client
 
 DATABASE_URL="https://yldvdgyehndhqchnvjis.supabase.co"
 DATABASE_ANON="sb_publishable_kX5MogJvs5ekIrSr-v1rjQ_vqsUdaUd"
-
-url: str = os.environ.get(DATABASE_URL)
-key: str = os.environ.get(DATABASE_ANON)
-supabase: Client = create_client(url, key)
+supabase: Client = create_client(DATABASE_URL, DATABASE_ANON)
 
 app = FastAPI()
 
