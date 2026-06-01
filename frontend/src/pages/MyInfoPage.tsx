093a53b390aed12f5e353ebf3db855aebe4ef298
Minrinjin <128560360+Minrinjin@users.noreply.github.com>
2026-06-01 14:00:47 -0400

    qualifications data structure WIP

--- frontend/src/pages/MyInfoPage.tsx
+++ frontend/src/pages/MyInfoPage.tsx
@@ -1,6 +1,44 @@
 import React from 'react';
 
+class Details {
+
+}
+class SkillDetails {
+    
+}
+class ExperienceDetails {
+    organization: String = ""
+    type period = {startMonth: Date, endMonth: Date}
+}
+
+class Qualification {
+    id: String | undefined;
+    name: String = "";
+    description: String = "";
+    type: "skill" 
+        | "experience" 
+        | "education" 
+        | "certification" 
+        | "project"
+        | undefined;
+    details: Details | undefined;
+}
+
+
+
+class Education extends Qualification {
+    institution: String = "";
+}
+
+class UserQualifications {
+    constructor(qualifications: Array<Qualification>) {}
+
+}
+
 export default function MyInfoPage() {
+    // Qualification array will be retrieved from server
+
+    // IDEA: let users use LinkedIn AutoFill
     return (<>
         My Info
     </>)
