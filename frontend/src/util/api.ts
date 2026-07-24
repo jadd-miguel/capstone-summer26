1bbad6fe703bb7fce725fe1fb332561ad523cf38
Minrinjin <128560360+Minrinjin@users.noreply.github.com>
2026-07-24 13:24:47 -0400

    export get

--- frontend/src/util/api.ts
+++ frontend/src/util/api.ts
@@ -50,7 +50,7 @@ export const del = async (url: string, body: any, responseFormat: ResponseFormat
 	return handleResponse(response, responseFormat);
 };
 
-const get = async (url: string, id:any, responseFormat: ResponseFormat = "json"): Promise<any> => {
+export const get = async (url: string, id:any, responseFormat: ResponseFormat = "json"): Promise<any> => {
 	let response = await fetch(`${url}?user_id=${id}`);
 	return handleResponse(response, responseFormat);
 };
