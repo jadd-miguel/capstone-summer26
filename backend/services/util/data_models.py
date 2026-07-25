bb54e67f2ec6600ca00f4d1eb656b5a28cb36f1c
jadd-miguel <i6hx3m@gmail.com>
2026-07-25 17:51:01 -0400

    revert model defaults

--- backend/services/util/data_models.py
+++ backend/services/util/data_models.py
@@ -40,18 +40,34 @@ class SupabaseError(HTTPException):
 
 class SkillGapInput:
     def __init__(self, payload):
-        self.candidate_skills = payload["candidate_skills"]
-        self.job_requirements = payload["job_requirements"]
+        if not payload:
+            self.candidate_skills = ["Python", "SQL", "Pandas"]
+            self.job_requirements = ["Python", "SQL", "Pandas", "Jira", "Tableau"]
+        else:
+            self.candidate_skills = payload["candidate_skills"]
+            self.job_requirements = payload["job_requirements"]
 
 class CoverLetterGenInput:
     def __init__(self, payload):
-        self.candidate_skills = payload["candidate_skills"]
-        self.job_title = payload["job_title"]
-        self.company_name = payload["company_name"]
+        if not payload:
+            self.candidate_skills = ["Python", "SQL", "Pandas", "Jira", "Tableau"]
+            self.job_title = "Software Developer"
+            self.company_name = "Fanshawe College"
+        else:
+            self.candidate_skills = payload["candidate_skills"]
+            self.job_title = payload["job_title"]
+            self.company_name = payload["company_name"]
 
 class ResumeGenInput:
     def __init__(self, payload):
-        self.candidate_name = payload["candidate_name"]
-        self.candidate_skills = payload["candidate_skills"]
-        self.experience_history = payload["experience_history"]
-        self.target_job_title = payload["target_job_title"]
+        if not payload:
+            self.candidate_name = "Victor Wembanyama"
+            self.candidate_skills = ["Python", "SQL", "Pandas", "Jira", "Tableau"]
+            self.experience_history = ["Sales Associate", "Teacher", "Automotive Mechanic"]
+            self.target_job_title = "Software Developer"
+
+        else:
+            self.candidate_name = payload["candidate_name"]
+            self.candidate_skills = payload["candidate_skills"]
+            self.experience_history = payload["experience_history"]
+            self.target_job_title = payload["target_job_title"]
